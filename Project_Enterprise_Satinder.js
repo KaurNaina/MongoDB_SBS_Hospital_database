2a9cb897fc4af74da8d29c533bfffbc17b07b314
KaurNaina <46692995+KaurNaina@users.noreply.github.com>
2019-12-16 14:31:35 -0500

    Creation Of Disease table..

--- Project_Enterprise_Satinder.js
+++ Project_Enterprise_Satinder.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Simar:Sunny123gg@cluster0-xbxcz.mongodb.net/test";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
 client.connect(err => {
-  const collection = client.db("SBS_Hospital").collection("Laboratory_Staff");
+  /*const collection = client.db("SBS_Hospital").collection("Laboratory_Staff");
   
   var insertLab=[{Staff_ID:501,FullName:"Rajni Kant",Gender:"M",Position:"Lab Technician",SalaryPH:20,SSN:917899024,Email:"rjk910@sbshosp.com",Phone:9102907831},
 				{Staff_ID:502,FullName:"Kartik Aryan",Gender:"M",Position:"Clinical Manager",SalaryPH:19.75,SSN:9182938054,Email:"karyan159@sbshosp.com",Phone:1009876492},
@@ -14,6 +14,20 @@ client.connect(err => {
 		if(err) throw err;
 		
 	console.log("Inserted 5 rows into Laboratory_Staff table..");
+  */
+  
+  
+	const collection = client.db("SBS_Hospital").collection("Disease");
+	var insertDisease=[{Disease_ID:"15D",Disease_Name:"Eczema",Drug_ID:15001},
+					{Disease_ID:"16D",Disease_Name:"Psoriasis",Drug_ID:15002},
+					{Disease_ID:"17D",Disease_Name:"Agoraphobia",Drug_ID:15003},
+					{Disease_ID:"18D",Disease_Name:"Dissociative Amnesia",Drug_ID:15004},
+					{Disease_ID:"19D",Disease_Name:"Anemia",Drug_ID:15005}];
+						
+	collection.insertMany(insertDisease,function(err,result){
+		if(err) throw err;
+		
+	console.log("Inserted 5 rows into Disease table..");
   
   client.close();
 });
